ea34fe281fe182936c887e0babf1445b91456313
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-19 00:43:39 +0100

    2 server

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
-		log.Println("Hello, micron server")
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("Hello world")
+	})
+
+	http.HandleFunc("/goodbye", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("Goodbye world")
 	})
 
 	http.ListenAndServe(":8080", nil)
