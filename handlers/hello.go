b190395bfdcb92e56fcb6e05ca666be68d718c4e
MacBobby Chibuzor <theghostmac@gmail.com>
2022-06-03 00:15:54 +0100

    created a method in a handler dir

---
+++ handlers/hello.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+}
+
+func (h *Hello) ServeHTTP(rw http.ResponseWriter, ref *http.Request) {
+	log.Println("Hello world")
+	d, err := ioutil.ReadAll(ref.Body)
+	if err != nil {
+		http.Error(ref, "Oops", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(rw, "Hello %s", d)
+}
