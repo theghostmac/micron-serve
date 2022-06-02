e2b5222a8965d289dea77dd3a95f7663b4a4fc56
MacBobby Chibuzor <theghostmac@gmail.com>
2022-06-03 00:08:16 +0100

    new serve

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		log.Println("Hello world")
+	http.HandleFunc("/", func(write http.ResponseWriter, request *http.Request) {
+		log.Println("hello world!")
+		d, err := ioutil.ReadAll(request.Body)
+		if err != nil {
+			http.Error(write, "Oops", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(write, "Hello %s", d)
 	})
-
-	http.HandleFunc("/goodbye", func(writer http.ResponseWriter, request *http.Request) {
-		log.Println("Goodbye world")
-	})
-
-	http.ListenAndServe(":8080", nil)
 }
